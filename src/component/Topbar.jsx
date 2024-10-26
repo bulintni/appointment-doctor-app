31f046b0b34f042bed4b3932f485f63b67e31cba
bulintanom <bulintni@gmail.com>
2024-10-26 17:48:40 +0700

    Update

--- src/component/Topbar.jsx
+++ src/component/Topbar.jsx
@@ -9,7 +9,7 @@ function Topbar() {
       </div>
       <div className='flex'>
         <div className='bg-gray-400 px-5 py-2 rounded-md mr-2'>
-          <p className='text-red-500'>รายได้ทั้งหมด 0.00 บาท</p>
+          <p className='text-red-500 '>รายได้ทั้งหมด 0.00 บาท</p>
         </div>
         <button className='border-2 border-orange-300 px-2 text-orange-500 rounded-md'><ReplayIcon /></button>
       </div>
